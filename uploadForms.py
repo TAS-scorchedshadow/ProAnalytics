72aa9c2d6d8eb0c3e6d0842198e4d4a8ee7a7bc8
Henry Guo <suchpunsmanywow@gmail.com>
2020-08-07 00:24:06 +1000

    Added upload button that puts the uploaded files (currently can be any file) into the testJson folder

--- uploadForms.py
+++ uploadForms.py
@@ -1,16 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
+from wtforms import StringField, SubmitField, HiddenField, MultipleFileField, FileField
 from wtforms.fields.html5 import DateField, TimeField, EmailField
 from wtforms.validators import DataRequired
 
 
-# collect all shot information needed
+# form for uploading files
 class uploadForm(FlaskForm):
-    shooterName = SubmitField('Shooter name: ')
-    coachName = SubmitField('Coach name: ')
-    range = SubmitField('Range: ')
-    dist = SubmitField('Distance: ')
-    shots = SubmitField('')
-    # Not sure if the following is needed
-    weather = SubmitField('Weather: ')
-    wind = SubmitField('Wind: ')
+    file = MultipleFileField(u'Submit File')
+    submit = SubmitField("Submit: ")
