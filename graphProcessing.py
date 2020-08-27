86909428def69cb56902d3aedfb45573fc3273b5
Rishi Wig <rishi.wig@gmail.com>
2020-08-27 21:31:43 +1000

    Master V6 (Rishi) - Shifted processing of graphs over to the graphProcessing python file.

--- graphProcessing.py
+++ graphProcessing.py
@@ -44,3 +44,44 @@ def compareLine(listx, listy, listName):
     cLine.xaxis.major_label_orientation = pi / 4
     script, div = components(cLine)
     return script, div
+
+def compareBar():
+    allStudentsTotal= {"SMITH_John": [7, 12], "JACK_Bob": [8, 6], "LI_Reginald": [9, 33], "VETTEL_Seb": [10,13],"CHILTON_Max": [11,20], "SENNA_Bruno": [12,27] }
+    names = []
+    scores = []
+    year = []
+    colour_list = []
+    for i in allStudentsTotal:
+        names.append(i)
+        scores.append(int(allStudentsTotal[i][1]))
+        year.append(int(allStudentsTotal[i][0]))
+
+    for years in year:
+        if years == 7:
+            colour_list.append("blue")
+        if years == 8:
+            colour_list.append("red")
+        if years == 9:
+            colour_list.append("yellow")
+        if years == 10:
+            colour_list.append("green")
+        if years == 11:
+            colour_list.append("black")
+        if years == 12:
+            colour_list.append("orange")
+
+    p_vbar = figure(x_range=names, plot_height=600, plot_width=(1000), title="Standings",
+                    toolbar_location=None, tools="hover")
+    p_vbar.hover.tooltips = [("Name", "@names"),("Score", "@scores")]
+    p_vbar.vbar(names, top=(scores), color=colour_list, width=0.2)  # names are the x points, top of the bar graph creates the column from 0 on the y-axis, width specifies gap between the columns
+    p_vbar.xgrid.grid_line_color = None
+    p_vbar.vbar(names, scores, legend_label="Year 7", color="blue")
+    p_vbar.vbar(names, scores, legend_label="Year 8", color = "red")
+    p_vbar.vbar(names, scores, legend_label="Year 9", color="yellow")
+    p_vbar.vbar(names, scores, legend_label="Year 10", color="green")
+    p_vbar.vbar(names, scores, legend_label="Year 11", color="black")
+    p_vbar.vbar(names, scores, legend_label="Year 12", color="orange")
+    p_vbar.legend.title = 'Year Groups'
+    p_vbar.y_range.start = 0  # ensures that the y-axis begins at 0
+    script, div = components(p_vbar)
+    return script, div
