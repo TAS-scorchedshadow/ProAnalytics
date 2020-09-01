851eca33a02c7850048859e945257e24676678c0
Henry Guo <suchpunsmanywow@gmail.com>
2020-09-01 22:34:13 +1000

    Merging targetList stuff into master (part 1)

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def report():
     username = request.args.get('username')
     conn = sqlite3.connect('PARS.db')
     c = conn.cursor()
-    c.execute('SELECT * FROM shoots WHERE username=?', (username,))
+    c.execute('SELECT * FROM shoots WHERE username=?', ('dylan.huynh1',))
     shoots = c.fetchall()
+    print(shoots)
     for shoot in shoots:
         c.execute('SELECT * FROM shots WHERE shootID=?', (shoot[0], ))
         range = shoot[3]
@@ -64,7 +65,8 @@ def report():
             shots[row[-1]] = [row[5], row[3], row[6]]
         script, div = graphProcessing.drawTarget(shots, range, 228.8, (12.66, -32.5))
         target_list.append([('a' + str(shoot[0])), script, div])
-    return render_template('OPGG.html', target_list=target_list, script=script, div=div)
+    print(target_list)
+    return render_template('OPGG.html', target_list=target_list)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -142,50 +144,6 @@ def comparitiveBar():
 def page_not_found(e):
     return render_template('404.html')
 
-@app.route('/comparitiveBar')
-def comparitiveBar():
-
-    allStudentsTotal= {"SMITH_John": [7, 12], "JACK_Bob": [8, 6], "LI_Reginald": [9, 33], "VETTEL_Seb": [10,13],"CHILTON_Max": [11,20], "SENNA_Bruno": [12,27] }
-    names = []
-    scores = []
-    year = []
-    colour_list = []
-
-    for i in allStudentsTotal:
-        names.append(i)
-        scores.append(int(allStudentsTotal[i][1]))
-        year.append(int(allStudentsTotal[i][0]))
-
-    for years in year:
-        if years == 7:
-            colour_list.append("blue")
-        if years == 8:
-            colour_list.append("red")
-        if years == 9:
-            colour_list.append("yellow")
-        if years == 10:
-            colour_list.append("green")
-        if years == 11:
-            colour_list.append("black")
-        if years == 12:
-            colour_list.append("orange")
-
-    p_vbar = figure(x_range=names, plot_height=600, plot_width=(1000), title="Standings",
-                    toolbar_location=None, tools="hover")
-    p_vbar.hover.tooltips = [("Name", "@names"),("Score", "@scores")]
-    p_vbar.vbar(names, top=(scores), color=colour_list, width=0.2)  # names are the x points, top of the bar graph creates the column from 0 on the y-axis, width specifies gap between the columns
-    p_vbar.xgrid.grid_line_color = None
-    p_vbar.vbar(names, scores, legend_label="Year 7", color="blue")
-    p_vbar.vbar(names, scores, legend_label="Year 8", color = "red")
-    p_vbar.vbar(names, scores, legend_label="Year 9", color="yellow")
-    p_vbar.vbar(names, scores, legend_label="Year 10", color="green")
-    p_vbar.vbar(names, scores, legend_label="Year 11", color="black")
-    p_vbar.vbar(names, scores, legend_label="Year 12", color="orange")
-    p_vbar.legend.title = 'Year Groups'
-    p_vbar.y_range.start = 0  # ensures that the y-axis begins at 0
-    script, div = components(p_vbar)
-
-    return render_template('comparitive_bar.html', script=script, div=div)
 
 if __name__ == '__main__':
     app.run(debug=True)
